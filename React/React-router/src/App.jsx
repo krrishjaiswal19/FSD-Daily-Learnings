c01b420301753840f8f1c7cb04784c518271642a
Krrishjaiswal <krrishjais45678@gmail.com>
2026-03-08 07:38:13 +0000

    Complete-ReactRouting-Learning

--- React/React-router/src/App.jsx
+++ React/React-router/src/App.jsx
@@ -1,13 +1,17 @@
 
 import './App.css'
+
 import Nav from './Components/Nav'
+import Footer from './Components/Footer'
+import { outlet } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
     <Nav/>
-  
+    <outlet/>
+    <Footer/>
     </>
   )
 }
--- React/React-router/src/main.jsx
+++ React/React-router/src/main.jsx
@@ -1,10 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import Home from './Components/Home.jsx'
+import Contact from './Components/Contact.jsx'
+import About from './Components/About.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>,
+    children:[
+      {
+        index:true,
+        element:<Home/>
+      },
+      {
+        path:"about",
+        element:<About/>
+      },
+      {
+        path:"contact",
+        element:<Contact/>
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
